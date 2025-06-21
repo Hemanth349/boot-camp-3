1ea9e0946c89275e369945f93bf96a1a7d3fa9fe
Hemanth349 <Hemanthkumar.hk155@gmail.com>
2025-06-21 16:05:39 -0400

    Update main.tf

--- project-3-dr/project-3-dr/main.tf
+++ project-3-dr/project-3-dr/main.tf
@@ -51,6 +51,7 @@ module "storage" {
 module "DNS" {
   source       = "./DNS"
   primary_ip   = var.primary_ip
+  secondary_ip   = var.secondary_ip
   domain_name  = var.domain_name
   providers = {
     google = google.primary
