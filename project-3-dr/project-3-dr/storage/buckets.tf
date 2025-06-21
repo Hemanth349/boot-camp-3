bf621d28b9f7ed40f0a9bba266658cb1bcab96ab
Hemanth349 <Hemanthkumar.hk155@gmail.com>
2025-06-21 12:23:29 -0400

    Update buckets.tf

--- project-3-dr/project-3-dr/storage/buckets.tf
+++ project-3-dr/project-3-dr/storage/buckets.tf
@@ -1,20 +1,32 @@
 resource "google_storage_bucket" "bucket" {
   name     = "${var.bucket_name}-${var.region}"
   location = var.region
-  versioning { enabled = true }
+  versioning {
+    enabled = true
+  }
 }
 
 resource "google_storage_transfer_job" "replication" {
   name = "replication-job"
 
   transfer_spec {
-    gcs_data_source { bucket_name = var.primary_bucket }
-    gcs_data_sink   { bucket_name = var.secondary_bucket }
+    gcs_data_source {
+      bucket_name = var.primary_bucket
+    }
+    gcs_data_sink {
+      bucket_name = var.secondary_bucket
+    }
   }
 
   schedule {
-    schedule_start_date { year = 2025, month = 6, day = 20 }
-    start_time_of_day   { hours = 1 }
+    schedule_start_date {
+      year  = 2025
+      month = 6
+      day   = 20
+    }
+    start_time_of_day {
+      hours = 1
+    }
   }
 
   status = "ENABLED"
