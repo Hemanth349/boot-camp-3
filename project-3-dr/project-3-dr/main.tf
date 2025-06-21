49d60eea6c5ca4643e9136e916db4138542cc8e0
Hemanth349 <Hemanthkumar.hk155@gmail.com>
2025-06-21 12:15:35 -0400

    Update main.tf

--- project-3-dr/project-3-dr/main.tf
+++ project-3-dr/project-3-dr/main.tf
@@ -19,54 +19,45 @@ provider "google" {
   alias   = "secondary"
 }
 
-# Primary VPC module deployed with primary provider
 module "vpc_primary" {
-  source  = "./project-3-dr/vpc"
+  source  = "./vpc"
   region  = var.primary_region
   index   = 1
-
   providers = {
     google = google.primary
   }
 }
 
-# Secondary VPC module deployed with secondary provider
 module "vpc_secondary" {
-  source  ="./project-3-dr/vpc"
+  source  = "./vpc"
   region  = var.secondary_region
   index   = 2
-
   providers = {
     google = google.secondary
   }
 }
 
-# Storage module creates buckets in primary and secondary regions using primary provider
 module "storage" {
-  source           = "./project-3-dr/storage"
+  source           = "./storage"
   region           = var.primary_region
   bucket_name      = var.bucket_name
   primary_bucket   = "${var.bucket_name}-${var.primary_region}"
   secondary_bucket = "${var.bucket_name}-${var.secondary_region}"
-
   providers = {
     google = google.primary
   }
 }
 
-# DNS module - Create DNS Managed Zone and failover records for your domain
 module "DNS" {
-  source       = "./project-3-dr/dns"
+  source       = "./dns"
   primary_ip   = var.primary_ip
   secondary_ip = var.secondary_ip
   domain_name  = var.domain_name
-
   providers = {
     google = google.primary
   }
 }
 
-# Enable Firestore API on your project
 resource "google_project_service" "firestore" {
   project = var.project_ID
   service = "firestore.googleapis.com"
