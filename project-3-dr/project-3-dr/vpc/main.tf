7c74a42f8a33e0ea5ebb6d3a0d3550aa26fe082f
Hemanth349 <Hemanthkumar.hk155@gmail.com>
2025-06-21 12:30:45 -0400

    Update main.tf

--- project-3-dr/project-3-dr/vpc/main.tf
+++ project-3-dr/project-3-dr/vpc/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
 resource "google_compute_network" "vpc" {
   name = "vpc-${var.region}"
   auto_create_subnetworks = false
